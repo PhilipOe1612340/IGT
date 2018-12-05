0643702e0ac0ae61d13144a4ed5cdf0154047823
Unknown <philip.oesterlin@gmail.com>
2018-12-05 14:04:39 +0100

    use mock

--- src/store/state.js
+++ src/store/state.js
@@ -1,9 +1,9 @@
 export default {
   user: null,
   allUsers: [],
-  URL: "http://localhost",
+  URL: "https://e9110a23-870c-415a-b784-a7d47df7074c.mock.pstmn.io/api",
   Port: 6001,
-  portmode: true,
+  portmode: false,
   airports: [],
   bookings: [],
   returnFlight: [],
